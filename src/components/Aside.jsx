993cca1fe9575a458bc4a86ec1369821c1bfb8bb
MaganaTony <maganajarm@gmail.com>
2024-08-21 21:02:56 -0600

    Aside component fi

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -78,7 +78,7 @@ const Aside = () => {
             <div className="mb-4 ms-3">
                 <div className="flex justify-between">
                 <h2 className="text-lg font-semibold text-white">For You</h2>
-                <svg className="size-5 mt-1" width="100%" height="100%" version="1.1" viewBox="0 0 20 20" x="0px" y="0px" role="presentation" aria-hidden="true" focusable="false" class="ScIconSVG-sc-1q25cff-1 jpczqG"><g><path fill="#FFFFFF" d="M16 16V4h2v12h-2zM6 9l2.501-2.5-1.5-1.5-5 5 5 5 1.5-1.5-2.5-2.5h8V9H6z"></path></g></svg>
+                <svg className="size-[1.5rem] mt-1" version="1.1" viewBox="0 0 20 20" x="0px" y="0px" role="presentation" aria-hidden="true" focusable="false" ><g><path fill="#FFFFFF" d="M16 16V4h2v12h-2zM6 9l2.501-2.5-1.5-1.5-5 5 5 5 1.5-1.5-2.5-2.5h8V9H6z"></path></g></svg>
                 </div>
                 <div className="mt-2">
                     <div className="flex items-center mb-2">
